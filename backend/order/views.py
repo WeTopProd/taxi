d770fd75a119a832d99c29813370972f3429b635
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-05-24 13:03:02 +0300

    back

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
+from .pagination import CustomPagination
 from .models import Order
 from .serializers import OrderSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
+    pagination_class = CustomPagination
     serializer_class = OrderSerializer
