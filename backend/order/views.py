3ee3a43736162222b27826aa77de5785fc6a60b6
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-05-31 12:07:22 +0300

    back

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
+from .filter import OrderFilter
 from .pagination import CustomPagination
 from .models import Order
 from .serializers import OrderSerializer
@@ -9,4 +11,5 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     pagination_class = CustomPagination
     serializer_class = OrderSerializer
-
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = OrderFilter
