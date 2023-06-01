311b662b41dfd1247c90ade1aa131cfea10e2b83
denispan <6231990@gmail.com>
2023-06-01 17:23:29 +0300

    меняет порт на 3001

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './localStorageService';
 import { routes } from '../helpers/routes';
 
-export const HOST = 'http://127.0.0.1:8000/api';
+export const HOST = 'http://127.0.0.1:8001/api';
 
 export const $api = axios.create({
   baseURL: HOST,
