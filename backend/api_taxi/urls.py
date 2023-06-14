8703e8550bc2b2bf4b1c530bdce194bb7e53fea6
94R1K <dfdfdf.dfdf.1985@mail.ru>
2023-06-14 17:51:24 +0300

    fix

--- backend/api_taxi/urls.py
+++ backend/api_taxi/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('api/', include('order.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns = (
-        urlpatterns
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
+urlpatterns = (
+    urlpatterns
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
